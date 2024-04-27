39886ae599c3bc4299ddc858f1c91cdfa74fe822
Jhorman2003 <131841197+Jhorman2003@users.noreply.github.com>
2024-04-26 22:01:38 -0500

    Mesas y reservas index

---
+++ resources/views/Mesa/index.blade.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Listado de Mesas</title>
+  </head>
+  <body>
+  <div class="container">
+        <h1>Listado de Mesas</h1>
+        <a href="{{ route('mesas.create') }}" class="btn btn-success">Add</a>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Número</th>
+                    <th scope="col">Capacidad</th>
+                    <th scope="col">Ubicación</th>
+                    <th scope="col">Acciones </th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($mesas as $mesa)
+                <tr>
+                    <th scope="row">{{ $mesa->mesa_id }}</td>
+                    <td>{{ $mesa->Numero }}</td>
+                    <td>{{ $mesa->Capacidad }}</td>
+                    <td>{{ $mesa->Ubicacion }}</td>
+                    <td><span> Acciones </span> </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
+         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" 
+      integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,7 +15,8 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
+                </div>            
+
             </div>
 
             <!-- Settings Dropdown -->
